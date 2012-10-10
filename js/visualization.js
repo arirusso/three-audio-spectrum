d1b775e8e4aa52956376a8232d5790bfde0ba52d
Ari Russo <ari.russo@gmail.com>
2012-10-10 00:08:33 -0700

    first pass at makin somethin happen

--- js/visualization.js
+++ js/visualization.js
@@ -1,13 +1,8 @@
 function Visualizer(model, selector) {
+  this.model = model;
 	// set the scene size
-	var WIDTH = 400,
-	    HEIGHT = 300;
-
-	// set some camera attributes
-	var VIEW_ANGLE = 45,
-	    ASPECT = WIDTH / HEIGHT,
-	    NEAR = 0.1,
-	    FAR = 10000;
+	this.WIDTH = 1024;
+	this.HEIGHT = 600;
 
 	// get the DOM element to attach to
 	// - assume we've got jQuery to hand
@@ -16,57 +11,96 @@ function Visualizer(model, selector) {
 	// create a WebGL renderer, camera
 	// and a scene
 	this.renderer = new THREE.WebGLRenderer();
-	this.camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-	                                ASPECT,
-	                                NEAR,
-	                                FAR  );
 	this.scene = new THREE.Scene();
 
-	// the camera starts at 0,0,0 so pull it back
-	this.camera.position.z = 300;
-
 	// start the renderer
-	this.renderer.setSize(WIDTH, HEIGHT);
+	this.renderer.setSize(this.WIDTH, this.HEIGHT);
 
 	// attach the render-supplied DOM element
 	$container.append(this.renderer.domElement);
 
-	// create the sphere's material
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-	    color: 0xCC0000
-	});
+  this.initializeObjects();
+  this.initializeCamera();
+  this.initializeLight();	
+}
 
-	// set up the sphere vars
-	var radius = 50, segments = 16, rings = 16;
+Visualizer.prototype.initializeCamera = function() {
+	// set some camera attributes
+	var VIEW_ANGLE = 90,
+	    ASPECT = this.WIDTH / this.HEIGHT,
+	    NEAR = 0.1,
+	    FAR = 10000;
 
-	// create a new mesh with sphere geometry -
-	// we will cover the sphereMaterial next!
-	this.sphere = new THREE.Mesh(
-	   new THREE.SphereGeometry(radius, segments, rings),
-	   sphereMaterial);
+	this.camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
+	                                ASPECT,
+	                                NEAR,
+	                                FAR  );
 
-	// add the sphere to the scene
-	this.scene.add(sphere);
-
+	// the camera starts at 0,0,0 so pull it back
+	this.camera.position.z = 300;
 	// and the camera
 	this.scene.add(this.camera);
+}
 
-	// create a point light
+Visualizer.prototype.initializeLight = function() {
+  // create a point light
 	var pointLight = new THREE.PointLight( 0xFFFFFF );
 
 	// set its position
 	pointLight.position.x = 10;
 	pointLight.position.y = 50;
-	pointLight.position.z = 130;
+	pointLight.position.z = 200;
 
 	// add to the scene
 	this.scene.add(pointLight);
-  var vis = this;
-  requestAnimationFrame(function() { vis.render });
+}
+
+Visualizer.prototype.initializeObjects = function() {
+  this.objects = [];
+  	// create the sphere's material
+	var material = new THREE.MeshLambertMaterial({ color: 0x000099});
+
+  for (var i = 0; i <= 10; i++) {
+
+    var geometry = new THREE.CubeGeometry( 60, 60, 60, 1, 1, 1, material);
+
+	  var shape = new THREE.Mesh(geometry, material);
+
+    // set the geometry to dynamic
+    // so that it allow updates
+    shape.geometry.dynamic = true;
+
+    // changes to the vertices
+    shape.geometry.__dirtyVertices = true;
+
+    // changes to the normals
+    shape.geometry.__dirtyNormals = true;
+
+    shape.position.x = (i * 70) - 300;
+
+    this.objects.push(shape);
+  }
+
+	for (i in this.objects) {
+    this.scene.add(this.objects[i]);
+  }
 }
 
 Visualizer.prototype.render = function() {
+  var vis = this;
+  requestAnimationFrame(function() { vis.render() });
+  var len = this.model.data.length
+  if (len > 0) {
+    var modlen = len / this.objects.length
+    for (i in this.objects) {
+      var d = this.model.data[Math.floor(i * modlen)];
+      var dd = d / 1000;
+      //console.log(dd);
+      if (dd > 0) {
+        this.objects[i].rotation.x += dd;
+      }
+    }
+  }
 	// draw!
 	this.renderer.render(this.scene, this.camera);
 
